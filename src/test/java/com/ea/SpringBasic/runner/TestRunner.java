605c96fb993b1a3ca17671553cf7bebb78472e0b
moham <mohammad.asif2008@hotmail.com>
2022-04-21 09:45:42 -0500

    report fixed

--- src/test/java/com/ea/SpringBasic/runner/TestRunner.java
+++ src/test/java/com/ea/SpringBasic/runner/TestRunner.java
@@ -7,8 +7,8 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = {"src/test/java/com/ea/SpringBasic/features"},
-       // plugin = {"json:target/cucumber-report.json", "pretty"},
-        plugin = {"target/cucumber-report-html/cucumber-report","pretty"},
+        plugin = {"json:target/cucumber-report-html/cucumber-html-report", "pretty"},
+        //plugin = {"target/cucumber-report-html/cucumber-report","pretty"},
         glue = "com.ea.SpringBasic.steps",
         tags = "@PAGE"
 )
